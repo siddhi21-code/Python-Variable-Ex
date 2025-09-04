02b5edcd0d1d0c58e472d3a345d26a63b6ee4cf2
siddhi21-code <siddhishirke508@gmail.com>
2025-09-04 17:14:31 +0530

    ok

---
+++ demo2.py
@@ -0,0 +1,21 @@
+car = "Thar"
+print(car)
+
+height = 2.0
+print(height)
+
+age = 14 
+print(age)
+
+is_student = True
+print(is_student)
+
+is_car = False
+print(is_car)
+
+
+cars =["Thar", "Bmw","audi"]
+print(cars)
+
+numbers =(125243 ,876649,47844)
+print(numbers)
